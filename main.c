79731491df94f9ab6a5a62a1ccf64cff6b8db48a
Malak Qeedan <75528759+Malak47@users.noreply.github.com>
2022-11-16 23:15:50 +0200

    Update main.c

--- main.c
+++ main.c
@@ -18,28 +18,28 @@ int main() {
         maxNum = num1;
         minNum = num2;
     }
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for(int i= minNum ; i<=maxNum ; i++){
         if(isArmstrong(i)==1){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Palindromes are: ");
+    printf("\nThe Palindromes are:");
     for(int i= minNum ; i<=maxNum ; i++){
         if(isPalindrome(i)==1){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for(int i= minNum ; i<=maxNum ; i++){
         if(isPrime(i)==1){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for(int i= minNum ; i<=maxNum ; i++){
         if(isStrong(i)==1){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
 
